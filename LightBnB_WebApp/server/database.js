db3a57cbe90eb43806439b28c11a43c385dbd2ec
tbalmeida <tbalmeida@gmail.com>
2020-02-06 16:40:49 +0000

    Renamed the LightBnB webapp folder

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -71,7 +71,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `SELECT r.property_id, p.owner_id, p.title, p.description, p.thumbnail_photo_url, p.cover_photo_url, 
+    p.cost_per_night, p.parking_spaces, p.number_of_bathrooms, p.number_of_bedrooms, 
+    p.country, p.street, p.city, p.province, p.post_code, p.active,
+    r.id reservation_id, cost_per_night, start_date, end_date, AVG(rating) average_rating
+    FROM reservations r
+    INNER JOIN properties p on r.property_id = p.id
+    INNER JOIN property_reviews pr ON pr.property_id = p.id AND pr.guest_id = r.guest_id
+    WHERE r.guest_id = $1
+      AND end_date <= now()::date
+    GROUP BY r.id, p.id
+    ORDER BY start_date DESC
+    LIMIT $2;`
+  return pool.query(query, [guest_id, limit])
+  .then(res => res.rows[0])
+  .catch(err => null)
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
